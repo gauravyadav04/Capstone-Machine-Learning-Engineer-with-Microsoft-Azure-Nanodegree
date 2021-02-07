77dda1be19537c4242705c00457a01299d4dee92
Gaurav <gaurav.yadav04@gmail.com>
2021-02-07 12:30:06 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -12,20 +12,26 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 from azureml.core import Dataset
 from azureml.core import Workspace
 
-subscription_id = 'f9d5a085-54dc-4215-9ba6-dad5d86e60a0'
-resource_group = 'aml-quickstarts-137804'
-workspace_name = 'quick-starts-ws-137804'
+# subscription_id = 'f9d5a085-54dc-4215-9ba6-dad5d86e60a0'
+# resource_group = 'aml-quickstarts-137804'
+# workspace_name = 'quick-starts-ws-137804'
 
-workspace = Workspace(subscription_id, resource_group, workspace_name)
+# workspace = Workspace(subscription_id, resource_group, workspace_name)
 
-dataset = Dataset.get_by_name(workspace, name='Heart_Failure_Clinical_Records_Dataset')
+# dataset = Dataset.get_by_name(workspace, name='Heart_Failure_Clinical_Records_Dataset')
+
+ws = Workspace.from_config()
+
+dataset = Dataset.get_by_name(ws, name='Heart_Failure_Clinical_Records_Dataset')
+
+df = dataset.to_pandas_dataframe()
 
 df = dataset.to_pandas_dataframe()
 
 x = df.drop(columns=['DEATH_EVENT'])
 y = df['DEATH_EVENT']
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.33, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
 run = Run.get_context()
 
 def main():
